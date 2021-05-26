e47f661b9eb753b150bb10133bc870f9646a3c60
AHrichi <8ahmed.hrichi8@gmail.com>
2021-05-26 10:46:42 +0100

    RHC

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddMovie from './components/AddMovie';
+import MovieList from './components/MovieList';
 
 function App() {
+  
+  const [movies, setMovies] = useState([
+    {
+      title: "Game of thrones",
+      description: "Genre : Fantasy",
+      posterUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/61r-vt0bLZL._AC_SX522_.jpg",
+      rate: "3",
+    },
+    {
+      title: "Breaking bad",
+      description:
+        "Walter White, 50 ans, est professeur de chimie dans un lycée du Nouveau-Mexique.",
+      posterUrl:
+        "https://fr.web.img5.acsta.net/pictures/19/06/18/12/11/3956503.jpg",
+      rate: "4",
+    },
+    {
+      title: "hunter x hunter",
+      description:
+        "Gon décide à l âge de 12 ans de partir pour devenir un Hunter.",
+      posterUrl:
+        "https://fr.web.img5.acsta.net/pictures/19/08/01/09/52/4803203.jpg",
+      rate: "5",
+    },
+
+  ])
+  const add=(addMovie)=>{
+    setMovies([...movies,addMovie])
+  }
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <MovieList movies={movies}/>
+        <AddMovie add={add}/>
       </header>
     </div>
   );
